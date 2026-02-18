68b268d06dbb6c9d0f296b280350afac7598f17a
TecCactus <dm0567814@gmail.com>
2026-02-18 04:20:36 +0000

    Use checker expected schema

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,8 @@
 -- 0. We are all unique!
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (id),
+  UNIQUE (email)
 );
