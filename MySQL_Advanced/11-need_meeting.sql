b0c12eefd0ef3d53be273b2061e7e0a0e0c42df7
TecCactus <dm0567814@gmail.com>
2026-02-18 04:37:38 +0000

    Add task 11 need_meeting view

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- 11. No table for a meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+  AND (
+    last_meeting IS NULL
+    OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
+  );
